762f29533d0d0053b19ad1f68a640c58d2689bb6
Leonid Belikov <mail@arfeo.net>
2019-12-29 21:24:55 +0300

    classes move to lib dir

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-
-const NpmAddDependencies = require('./NpmAddDependencies');
+const NpmAddDependencies = require('./lib/NpmAddDependencies');
 
 console.log('\x1b[33m%s\x1b[0m', 'This script adds dependencies (latest or specified versions) to the package.json file without installing them');
 
