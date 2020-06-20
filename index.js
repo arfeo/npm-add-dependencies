2af5ad1581da56bc83a5e23dc66c7784df4552f2
jack <jackc@strangeanimals.com.au>
2020-06-20 13:34:32 +0800

    hmm

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 #!/usr/bin/env node
-module.exports = require('./lib/AddDependencies');
+const AddDependencies = require('./lib/AddDependencies');
+
+console.log(
+  '\x1b[33m%s\x1b[0m',
+  'This script adds dependencies (latest or specified versions) to the package.json file skipping the installation process.'
+);
+
+const app = new AddDependencies();
+
+app
+  .addDependencies()
+  .then(app.saveToPackage.bind(app))
+  .catch((error) => {
+    console.error('\x1b[31m%s\x1b[0m', error);
+    process.exit(1);
+  });
