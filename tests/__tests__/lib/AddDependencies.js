c88c5fd14bb2065f75f71ad93945d8785852da69
jack <jackc@strangeanimals.com.au>
2020-06-22 22:30:35 +0800

    Files tests

--- tests/__tests__/lib/AddDependencies.js
+++ tests/__tests__/lib/AddDependencies.js
@@ -1,3 +1,5 @@
-test('first test', () => {
-  expect(true).toBeTruthy();
+describe('test run()', () => {
+  test('This doesnt need to be tested as its tested by the index and cli-index tests', () => {
+    expect(true).toBeTruthy();
+  });
 });
